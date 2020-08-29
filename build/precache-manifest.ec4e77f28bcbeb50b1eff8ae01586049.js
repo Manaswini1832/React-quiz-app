28c8ed81473cccb2701e0cb817997433a674052c
Manaswini <58681405+Manaswini1832@users.noreply.github.com>
2020-08-29 18:07:56 +0530

    Initial commit

---
+++ build/precache-manifest.ec4e77f28bcbeb50b1eff8ae01586049.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0583cda7924eb31b872f327d4cb798d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a0c0acaedbf11d678a9",
+    "url": "/static/css/main.ec4ec61e.chunk.css"
+  },
+  {
+    "revision": "7f623081b87268b14ef9",
+    "url": "/static/js/2.dfcdf592.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dfcdf592.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a0c0acaedbf11d678a9",
+    "url": "/static/js/main.81352aed.chunk.js"
+  },
+  {
+    "revision": "5a5a4fbe531f50699c38",
+    "url": "/static/js/runtime-main.a961fa62.js"
+  }
+]);
